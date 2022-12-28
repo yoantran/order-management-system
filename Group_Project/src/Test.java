7aedcaddb353fb373c811f4bc1e7e458aa148945
ToGiaHy <100851997+ToGiaHy@users.noreply.github.com>
2022-12-28 01:04:06 -0800

    Update Test.java

--- Group_Project/src/Test.java
+++ Group_Project/src/Test.java
@@ -4,7 +4,5 @@ import java.security.NoSuchAlgorithmException;
 
 public class Test {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        accounts ac1 = new accounts();
-        ac1.registerCustomerAccount();
     }
 }
