c4f963cebeb39f5d8dde0cf514a3ca42ecdd6758
minhnguyen200703 <101308871+minhnguyen200703@users.noreply.github.com>
2022-12-30 17:34:23 +0700

    Cart class added

--- src/Cart.java
+++ src/Cart.java
@@ -1,3 +1,42 @@
-public class Cart {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Cart {
+    private Map<Product, Integer> products;
+
+    public Cart() {
+        products = new HashMap<>();
+    }
+
+    public void addProduct(Product product, int amount) {
+        if (products.containsKey(product)) {
+            amount += products.get(product);
+        }
+        products.put(product, amount);
+    }
+
+    public void addProduct(Product product) {
+        addProduct(product, 1);
+    }
+
+
+    public void deleteProduct(Product product) {
+        products.remove(product);
+    }
+
+    public void reset() {
+        products.clear();
+    }
+
+
+
+//    Cart cart = new Cart();
+//Product product1 = new Product("Product 1", 50, "Category 1");
+//Product product2 = new Product("Product 2", 100, "Category 2");
+//cart.addProduct(product1, 2);
+//cart.addProduct(product2, 3);
+
+//    cart.deleteProduct(product1);
+
+//    cart.reset();
 }
