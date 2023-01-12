8befd13d58b82ca4914099dc541fe6195fc11070
ToGiaHy <100851997+ToGiaHy@users.noreply.github.com>
2023-01-12 21:29:39 +0700

    Create a cart class by using the variable Map to contain the products

---
+++ Group_Project/src/Cart.java
@@ -0,0 +1,98 @@
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+    public class Cart {
+        private Map<String[], Integer> products;
+        private double totalAmount;
+
+        public Cart() {
+            products = new HashMap<>();
+            totalAmount = 0;
+        }
+
+        public void addProduct(String product, int amount) throws IOException {
+            if (products.containsKey(product)) {
+                amount += products.get(product);
+            }
+            Product newProduct = Product.findProductById(product);
+            String[] productInfo = new String[]{newProduct.getId(), newProduct.getProductName(), String.valueOf(newProduct.getPrice())};
+            products.put(productInfo, amount);
+            totalAmount += newProduct.getPrice() * amount;
+        }
+
+        public void addProduct(String product, int amount, int price, String productName) {
+            if (products.containsKey(product)) {
+                amount += products.get(product);
+            }
+            String[] productInfo = new String[]{product, productName, String.valueOf(price)};
+            products.put(productInfo, amount);
+            totalAmount += price * amount;
+        }
+
+        public void deleteProductById(String productId) {
+            String[] productToRemove = null;
+            for (Map.Entry<String[], Integer> entry : products.entrySet()) {
+                String[] productInfo = entry.getKey();
+                if (productInfo[0].equals(productId)) {
+                    productToRemove = productInfo;
+                    break;
+                }
+            }
+            if (productToRemove != null) {
+                int amount = products.get(productToRemove);
+                totalAmount -= amount * Double.parseDouble(productToRemove[2]);
+                products.remove(productToRemove);
+            }
+        }
+
+        public void deleteProductByName(String productName) {
+            String[] productToRemove = null;
+            for (Map.Entry<String[], Integer> entry : products.entrySet()) {
+                String[] productInfo = entry.getKey();
+                if (productInfo[1].equals(productName)) {
+                    productToRemove = productInfo;
+                    break;
+                }
+            }
+            if (productToRemove != null) {
+                int amount = products.get(productToRemove);
+                totalAmount -= amount * Double.parseDouble(productToRemove[2]);
+                products.remove(productToRemove);
+            }
+        }
+
+        public void reset() {
+            products.clear();
+            totalAmount = 0;
+        }
+
+
+        public double getTotalAmount() {
+            return totalAmount;
+        }
+
+        public Map<String[], Integer> getProducts() {
+            return products;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<String[], Integer> entry : products.entrySet()) {
+                String[] productInfo = entry.getKey();
+                int amount = entry.getValue();
+                sb.append(productInfo[0]).append(";").append(productInfo[1]).append(";")
+                        .append(productInfo[2]).append(";").append(amount).append("|");
+            }
+            int length = sb.length();
+            if (length > 0) {
+                sb.deleteCharAt(length - 1);
+            }
+            return sb.toString();
+        }
+    }
+
+
